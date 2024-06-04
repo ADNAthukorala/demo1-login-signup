3822e7c46c7d9c90b8fe1a7dc5802dd67d01ba8c
Dumindu Athukorala <117508037+ADNAthukorala@users.noreply.github.com>
2024-06-04 10:59:54 +0530

    Changed

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -3,16 +3,27 @@ import './LoginSignup.css';
 import user_icon from '../Assets/user.png';
 import email_icon from '../Assets/email.png';
 import password_icon from '../Assets/password.png';
+import { useState } from 'react';
 
 const LoginSignup = () => {
+    const [action, setAction] = useState('Sign Up');
+
+    function signup() {
+        setAction('Sign Up');
+    }
+
+    function login() {
+        setAction('Login');
+    }
+
   return (
     <div className='container'>
         <div className='header'>
-            <div className='text'>Sign Up</div>
+            <div className='text'>{action}</div>
             <div className='underline'></div>
         </div>
-        <div className='inputs'>
-            <div className='input'>
+        <div className={'inputs'}>
+            <div className={action === 'Sign Up' ? 'input' : 'input hide'}>
                 <img src={user_icon} alt='' />
                 <input type='text' placeholder='Name' />
             </div>
@@ -25,10 +36,10 @@ const LoginSignup = () => {
                 <input type='password' placeholder='Password' />
             </div>
         </div>
-        <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
-        <div className="submit-container">
-            <div className="submit">Sign Up</div>
-            <div className="submit">Login</div>
+        <div className={action === 'Sign Up' ? 'forgot-password hide' : 'forgot-password'}>Lost Password? <span>Click Here!</span></div>
+        <div className='submit-container'>
+            <button type='button' className={action === 'Sign Up' ? 'submit' : 'submit gray'} onClick={signup}>Sign Up</button>
+            <button type='button' className={action === 'Login' ? 'submit' : 'submit gray'} onClick={login}>Login</button>
         </div>
     </div>
   );
